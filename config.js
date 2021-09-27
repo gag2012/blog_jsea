a5d4881d16540e229cec798227ffb5daaba1b6c8
@Jsea <40004210+gag2012@users.noreply.github.com>
2021-09-28 02:23:58 +0900

    Update config.js

--- config.js
+++ config.js
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: '@Jsea',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Infra, Network, IT engineer',
     contacts: {
       email: '',
       facebook: '#',
